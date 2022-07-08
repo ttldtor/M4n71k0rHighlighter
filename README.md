7ed6b98657740f952f2f3a5c9077be856e2cf141
Anatoly Kalin <ttldtor@gmail.com>
2022-07-09 00:09:18 +0300

    First

---
+++ README.md
@@ -0,0 +1,8 @@
+# M4n71k0r Highlighter 
+
+[![Release](https://img.shields.io/github/v/release/ttldtor/M4n71k0rHighlighter)](https://github.com/ttldtor/M4n71k0rHighlighter/releases/latest)
+[![License](https://img.shields.io/badge/license-Unlicense-orange)](https://github.com/ttldtor/WHAT-S-HAPPENING-/blob/master/LICENSE)
+
+**The extension that allows you to highlight the code, taking into account the current theme, using "hints" (Markdown or other) or without "hints".**
+
+
---
+++ src/m4n71k0rhghlghtr.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name        M4n71k0r Highlighter
+// @namespace   https://github.com/ttldtor
+// @match       *://*
+// @include     *://*
+// @grant       none
+// @version     0.1.0
+// @author      ttldtor
+// @description The extension that allows you to highlight the code, taking into account the current theme, using "hints" (Markdown or other) or without "hints".
+// @license     Unlicense
+// @homepageURL https://github.com/ttldtor/M4n71k0rHighlighter
+// @downloadURL https://raw.githubusercontent.com/ttldtor/ManticoreHighlighter/default/src/m4n71k0rhghlghtr.user.js
+// @updateURL   https://raw.githubusercontent.com/ttldtor/ManticoreHighlighter/default/src/m4n71k0rhghlghtr.user.js
+// ==/UserScript==
+
+(function(){
+  'use strict';
+  
+  function findCode() {
+	return [];
+  }
+  
+  function update() {
+    setInterval(update, 1000)
+  }
+  
+  setInterval(update, 500);  
+})();
